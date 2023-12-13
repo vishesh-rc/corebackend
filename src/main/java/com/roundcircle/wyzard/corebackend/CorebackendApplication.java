ba19b7f10706b32d4602d3574b3614e3b662750e
Vishesh Kaul <visheshkaul@192.168.1.4>
2023-12-13 15:15:27 +0530

    Initial commit

---
+++ src/main/java/com/roundcircle/wyzard/corebackend/CorebackendApplication.java
@@ -0,0 +1,13 @@
+package com.roundcircle.wyzard.corebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roundcircle/wyzard/corebackend/CorebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roundcircle.wyzard.corebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
